31e04b33c36b55297b8f60e7c68c9a7532ddc9e7
Flobeney <flobeney@users.noreply.github.com>
2020-11-25 15:11:47 +0100

    Couper une image en 4 et la réarranger

---
+++ C#/transformMitch/transformMitch/FrmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace transformMitch {
+    partial class FrmMain {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.gbImgOriginal = new System.Windows.Forms.GroupBox();
+            this.pibImgOriginal = new System.Windows.Forms.PictureBox();
+            this.gbImgRes = new System.Windows.Forms.GroupBox();
+            this.pibImgRes = new System.Windows.Forms.PictureBox();
+            this.ofdImg = new System.Windows.Forms.OpenFileDialog();
+            this.btnChooseImg = new System.Windows.Forms.Button();
+            this.btnTransform = new System.Windows.Forms.Button();
+            this.sfdImg = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.gbImgOriginal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pibImgOriginal)).BeginInit();
+            this.gbImgRes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pibImgRes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbImgOriginal
+            // 
+            this.gbImgOriginal.Controls.Add(this.pibImgOriginal);
+            this.gbImgOriginal.Location = new System.Drawing.Point(13, 13);
+            this.gbImgOriginal.Margin = new System.Windows.Forms.Padding(4);
+            this.gbImgOriginal.Name = "gbImgOriginal";
+            this.gbImgOriginal.Padding = new System.Windows.Forms.Padding(4);
+            this.gbImgOriginal.Size = new System.Drawing.Size(533, 431);
+            this.gbImgOriginal.TabIndex = 1;
+            this.gbImgOriginal.TabStop = false;
+            this.gbImgOriginal.Text = "Image original";
+            // 
+            // pibImgOriginal
+            // 
+            this.pibImgOriginal.Location = new System.Drawing.Point(8, 23);
+            this.pibImgOriginal.Margin = new System.Windows.Forms.Padding(4);
+            this.pibImgOriginal.Name = "pibImgOriginal";
+            this.pibImgOriginal.Size = new System.Drawing.Size(517, 400);
+            this.pibImgOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pibImgOriginal.TabIndex = 0;
+            this.pibImgOriginal.TabStop = false;
+            // 
+            // gbImgRes
+            // 
+            this.gbImgRes.Controls.Add(this.pibImgRes);
+            this.gbImgRes.Location = new System.Drawing.Point(554, 13);
+            this.gbImgRes.Margin = new System.Windows.Forms.Padding(4);
+            this.gbImgRes.Name = "gbImgRes";
+            this.gbImgRes.Padding = new System.Windows.Forms.Padding(4);
+            this.gbImgRes.Size = new System.Drawing.Size(533, 431);
+            this.gbImgRes.TabIndex = 2;
+            this.gbImgRes.TabStop = false;
+            this.gbImgRes.Text = "Résultat";
+            // 
+            // pibImgRes
+            // 
+            this.pibImgRes.Location = new System.Drawing.Point(8, 23);
+            this.pibImgRes.Margin = new System.Windows.Forms.Padding(4);
+            this.pibImgRes.Name = "pibImgRes";
+            this.pibImgRes.Size = new System.Drawing.Size(517, 400);
+            this.pibImgRes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pibImgRes.TabIndex = 0;
+            this.pibImgRes.TabStop = false;
+            // 
+            // ofdImg
+            // 
+            this.ofdImg.FileName = "img";
+            this.ofdImg.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
+            this.ofdImg.Title = "Choisir une image";
+            // 
+            // btnChooseImg
+            // 
+            this.btnChooseImg.Location = new System.Drawing.Point(13, 452);
+            this.btnChooseImg.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChooseImg.Name = "btnChooseImg";
+            this.btnChooseImg.Size = new System.Drawing.Size(133, 37);
+            this.btnChooseImg.TabIndex = 3;
+            this.btnChooseImg.Text = "Choisir une image";
+            this.btnChooseImg.UseVisualStyleBackColor = true;
+            this.btnChooseImg.Click += new System.EventHandler(this.btnChooseImg_Click);
+            // 
+            // btnTransform
+            // 
+            this.btnTransform.Location = new System.Drawing.Point(405, 452);
+            this.btnTransform.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTransform.Name = "btnTransform";
+            this.btnTransform.Size = new System.Drawing.Size(133, 37);
+            this.btnTransform.TabIndex = 4;
+            this.btnTransform.Text = "Transformation";
+            this.btnTransform.UseVisualStyleBackColor = true;
+            this.btnTransform.Click += new System.EventHandler(this.btnTransform_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(554, 452);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 37);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Sauver";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 499);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnTransform);
+            this.Controls.Add(this.btnChooseImg);
+            this.Controls.Add(this.gbImgRes);
+            this.Controls.Add(this.gbImgOriginal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transformation";
+            this.gbImgOriginal.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pibImgOriginal)).EndInit();
+            this.gbImgRes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pibImgRes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbImgOriginal;
+        private System.Windows.Forms.PictureBox pibImgOriginal;
+        private System.Windows.Forms.GroupBox gbImgRes;
+        private System.Windows.Forms.PictureBox pibImgRes;
+        private System.Windows.Forms.OpenFileDialog ofdImg;
+        private System.Windows.Forms.Button btnChooseImg;
+        private System.Windows.Forms.Button btnTransform;
+        private System.Windows.Forms.SaveFileDialog sfdImg;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ C#/transformMitch/transformMitch/FrmMain.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace transformMitch {
+    public partial class FrmMain : Form {
+        //Constantes
+        const int NB_CORNERS = 4;
+        const string BASE_FILE = "../../../Mitch.jpg";
+
+        //Champs
+        Bitmap imgBase;
+        Bitmap imgRes;
+
+        public FrmMain() {
+            InitializeComponent();
+            //Init
+            LoadPicture(BASE_FILE);
+        }
+
+        private void LoadPicture(string filename) {
+            imgBase = new Bitmap(filename);
+            imgRes = new Bitmap(imgBase.Width, imgBase.Height);
+            pibImgOriginal.Image = imgBase;
+        }
+
+        private void btnChooseImg_Click(object sender, EventArgs e) {
+            //Image choisie
+            if (ofdImg.ShowDialog() == DialogResult.OK) {
+                LoadPicture(ofdImg.FileName);
+            }
+        }
+
+        private void btnTransform_Click(object sender, EventArgs e) {
+            List<Bitmap> corners = new List<Bitmap>();
+            Random rnd = new Random();
+            int midSizeWidth = imgBase.Width / 2;
+            int midSizeHeight = imgBase.Height / 2;
+            Size cornerSize = new Size(midSizeWidth, midSizeHeight);
+
+            //Couper l'image en 4 parties
+            for (int i = 0; i < NB_CORNERS / 2; i++) {
+                for (int j = 0; j < NB_CORNERS / 2; j++) {
+                    corners.Add(CropRect(
+                        imgBase,
+                        new Rectangle(
+                            new Point(
+                                i * midSizeWidth,
+                                j * midSizeHeight
+                            ),
+                            cornerSize
+                        )
+                    ));
+                }
+            }
+
+            //Mélanger les images
+            corners = corners.OrderBy(x => rnd.Next()).ToList();
+
+            //Les copier sur l'image de résultat
+            for (int i = 0; i < NB_CORNERS / 2; i++) {
+                for (int j = 0; j < NB_CORNERS / 2; j++) {
+                    imgRes = DrawOnBitmap(
+                        corners.First(),
+                        imgRes,
+                        new Point(
+                            i * midSizeWidth,
+                            j * midSizeHeight
+                        )
+                    );
+                    corners.RemoveAt(0);
+                }
+            }
+
+            pibImgRes.Image = imgRes;
+        }
+
+        /// <summary>
+        /// Coupe une partie d'une image
+        /// </summary>
+        /// <param name="src">Image à couper</param>
+        /// <param name="r">Rectangle avec les dimensions à couper</param>
+        /// <returns>Une nouvelle image coupée selon les dimensions demandée</returns>
+        private Bitmap CropRect(Bitmap src, Rectangle r) {
+            //Créer l'image résultante
+            Bitmap res = new Bitmap(r.Width, r.Height);
+
+            //Dessiner sur l'image
+            using (Graphics g = Graphics.FromImage(res)) {
+                //Dessiner la partie de l'image voulue
+                g.DrawImage(src, -r.X, -r.Y);
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Dessiner la totalité de l'image source sur l'image de résultat à l'endroit donné
+        /// </summary>
+        /// <param name="src">Image source</param>
+        /// <param name="dst">Image de destination</param>
+        /// <param name="p">Endroit donné où dessiner dans l'image de résultat</param>
+        /// <returns></returns>
+        private Bitmap DrawOnBitmap(Bitmap src, Bitmap dst, Point p) {
+            using (Graphics g = Graphics.FromImage(dst)) {
+                g.DrawImage(src, p);
+            }
+
+            return dst;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            //Ouvrir fenêtre de sauvegarde
+            if (sfdImg.ShowDialog() == DialogResult.OK) {
+                string ext = Path.GetExtension(sfdImg.FileName);
+                //Format de l'image
+                ImageFormat format = ImageFormat.Png;
+                switch (ext) {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                //Sauver l'image
+                imgRes.Save(sfdImg.FileName, format);
+            }
+        }
+    }
+}
